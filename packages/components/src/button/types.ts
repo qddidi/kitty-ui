e412acd0210c24ff15289547b52046dc16e3b2ed
qddidi <2046353594@qq.com>
2022-07-07 00:07:59 +0800

    feat: 打包更换vite

---
+++ packages/components/src/button/types.ts
@@ -0,0 +1,22 @@
+
+import { ExtractPropTypes } from 'vue'
+
+
+export type ButtonType =
+  | 'default'
+  | 'primary'
+  | 'success'
+  | 'warning'
+  | 'danger';
+
+export type ButtonSize = 'large' | 'normal' | 'small' | 'mini';
+
+
+type buttonProps = {
+  type: ButtonType,
+  size: ButtonSize
+}
+
+export type ButtonProps = ExtractPropTypes<buttonProps>
+
+
