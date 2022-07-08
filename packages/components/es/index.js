e103a7caf27ae588800f6a3afa5e6728b2db44cc
张迪生 <2046353594@qq.com>
2022-07-08 17:13:27 +0800

    feat: 增加git忽略文件

--- packages/components/es/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Button } from "./button/index.js";
-export { default as Icon } from "./Icon/icon.js";
--- packages/components/lib/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperties(exports, { __esModule: { value: true }, [Symbol.toStringTag]: { value: "Module" } });
-var index = require("./button/index.js");
-var icon = require("./Icon/icon.js");
-exports.Button = index;
-exports.Icon = icon;
