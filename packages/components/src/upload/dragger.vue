5d4e1fb3592bd70d2cf31f8f053d1fa52f33ede5
张迪生 <2046353594@qq.com>
2022-09-06 18:22:02 +0800

    publish: 3.0.11

--- packages/components/src/upload/dragger.vue
+++ packages/components/src/upload/dragger.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="k-upload-dragger" :class="{ ['k-upload-draggerenter']: isEnter }" ref="fileArea"
-        @click="emits('fileUpload')">
-        <div class="k-upload-content">
-            <Icon class="k-upload-icon" :name="isEnter ? 'file-open' : 'folder-close'" />
-            <div class="k-upload-dragger-text">将文件拖到此处或<em>点击上传</em></div>
-        </div>
+  <div class="k-upload-dragger" :class="{ ['k-upload-draggerenter']: isEnter }" ref="fileArea"
+    @click="emits('fileUpload')">
+    <div class="k-upload-content">
+      <Icon class="k-upload-icon" :name="isEnter ? 'file-open' : 'folder-close'" />
+      <div class="k-upload-dragger-text">将文件拖到此处或<em>点击上传</em></div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -15,20 +15,20 @@ import Icon from '../Icon/icon.vue'
 const emits = defineEmits(['getFilesList', 'fileUpload'])
 const fileArea = ref()
 const isEnter = ref(false)
-const timer = ref(null);
+const timer = ref<null | number>(null);
 const filesList = ref<File[]>([])
 
 onMounted(() => {
-    fileArea.value.addEventListener('drop', (e: any) => {
-        e.preventDefault()
-        filesList.value.push(...Array.from(e.dataTransfer.files as FileList))
-        emits('getFilesList', filesList.value)
+  fileArea.value.addEventListener('drop', (e: any) => {
+    e.preventDefault()
+    filesList.value.push(...Array.from(e.dataTransfer.files as FileList))
+    emits('getFilesList', filesList.value)
 
-    }, false)
-    fileArea.value.addEventListener('dragover', onDragover, false);
+  }, false)
+  fileArea.value.addEventListener('dragover', onDragover, false);
 })
 
-const onDragover = (e:Event) => {
+const onDragover = (e: Event) => {
   isEnter.value = true;
   // dragover+防抖代替dragenter和dragleave，因为dragleave进入子元素也会触发
   if (timer.value !== null) {
