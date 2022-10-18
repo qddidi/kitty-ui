2a97061252ee1676a65ae9ed9774b8d37fd36428
qddidi <2046353594@qq.com>
2022-10-19 00:42:19 +0800

    feat: 完成shake组件

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -3,7 +3,6 @@ export * from './src/index'
 import { App } from 'vue'
 export default {
     install: (app: App) => {
-
         for (const comkey in components) {
             app.use((components as any)[comkey])
             //  app.component((components as any)[comkey].name, (components as any)[comkey])
